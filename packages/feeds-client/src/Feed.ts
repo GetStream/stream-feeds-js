0b9fce084a342adda3704a0f5ab13444ee61e37c
Anton Arnautov <arnautov.anton@gmail.com>
2025-06-17 19:21:29 +0200

    Cleanup Feed and FeedsClient, add handlers and helpers

--- packages/feeds-client/src/Feed.ts
+++ packages/feeds-client/src/Feed.ts
@@ -1,45 +1,9 @@
 import {
-  ActivityAddedEvent,
-  ActivityDeletedEvent,
-  ActivityMarkEvent,
-  ActivityPinnedEvent,
-  ActivityReactionAddedEvent,
-  ActivityReactionDeletedEvent,
-  ActivityRemovedFromFeedEvent,
   ActivityRequest,
-  ActivityUnpinnedEvent,
-  ActivityUpdatedEvent,
-  BookmarkAddedEvent,
-  BookmarkDeletedEvent,
-  BookmarkUpdatedEvent,
-  CommentAddedEvent,
-  CommentDeletedEvent,
-  CommentReactionAddedEvent,
-  CommentReactionDeletedEvent,
-  CommentUpdatedEvent,
-  FeedCreatedEvent,
-  FeedDeletedEvent,
-  FeedGroupChangedEvent,
-  FeedGroupDeletedEvent,
-  FeedMemberAddedEvent,
-  FeedMemberRemovedEvent,
-  FeedMemberUpdatedEvent,
   FeedResponse,
-  FeedUpdatedEvent,
-  FollowCreatedEvent,
-  FollowDeletedEvent,
-  FollowUpdatedEvent,
   GetOrCreateFeedRequest,
   GetOrCreateFeedResponse,
-  ModerationCustomActionEvent,
-  ModerationFlaggedEvent,
-  ModerationMarkReviewedEvent,
-  PollClosedFeedEvent,
-  PollDeletedFeedEvent,
-  PollUpdatedFeedEvent,
-  PollVoteCastedFeedEvent,
-  PollVoteChangedFeedEvent,
-  PollVoteRemovedFeedEvent,
+  QueryFollowsRequest,
   WSEvent,
 } from './gen/models';
 import { StateStore } from './common/StateStore';
@@ -71,14 +35,21 @@ export type FeedState = Partial<
     is_loading_activities: boolean;
   };
 
+type EventHandlerByEventType = {
+  [Key in NonNullable<WSEvent['type']>]: Key extends Extract<
+    WSEvent,
+    { type: Key }
+  >['type']
+    ? ((event: Extract<WSEvent, { type: Key }>) => void) | undefined
+    : never;
+};
+
 export class Feed extends FeedApi {
   readonly state: StateStore<FeedState>;
-  private readonly eventHandlers: {
-    [key in WSEvent['type']]: (_: WSEvent & { type: key }) => void;
-  } = {
-    'feeds.activity.added': (
-      event: { type: 'feeds.activity.added' } & ActivityAddedEvent,
-    ) => {
+  private static readonly noop = () => {};
+
+  private readonly eventHandlers: EventHandlerByEventType = {
+    'feeds.activity.added': (event) => {
       const currentActivities = this.state.getLatestValue().activities;
       const result = addActivitiesToState(
         [event.activity],
@@ -89,9 +60,7 @@ export class Feed extends FeedApi {
         this.state.partialNext({ activities: result.activities });
       }
     },
-    'feeds.activity.deleted': (
-      event: { type: 'feeds.activity.deleted' } & ActivityDeletedEvent,
-    ) => {
+    'feeds.activity.deleted': (event) => {
       const currentActivities = this.state.getLatestValue().activities;
       if (currentActivities) {
         const result = removeActivityFromState(
@@ -103,11 +72,7 @@ export class Feed extends FeedApi {
         }
       }
     },
-    'feeds.activity.reaction.added': (
-      event: {
-        type: 'feeds.activity.reaction.added';
-      } & ActivityReactionAddedEvent,
-    ) => {
+    'feeds.activity.reaction.added': (event) => {
       const currentActivities = this.state.getLatestValue().activities;
       const connectedUser = this.client.state.getLatestValue().connectedUser;
       const isCurrentUser = Boolean(
@@ -123,11 +88,7 @@ export class Feed extends FeedApi {
         this.state.partialNext({ activities: result.activities });
       }
     },
-    'feeds.activity.reaction.deleted': (
-      event: {
-        type: 'feeds.activity.reaction.deleted';
-      } & ActivityReactionDeletedEvent,
-    ) => {
+    'feeds.activity.reaction.deleted': (event) => {
       const currentActivities = this.state.getLatestValue().activities;
       const connectedUser = this.client.state.getLatestValue().connectedUser;
       const isCurrentUser = Boolean(
@@ -143,16 +104,8 @@ export class Feed extends FeedApi {
         this.state.partialNext({ activities: result.activities });
       }
     },
-    'feeds.activity.removed_from_feed': (
-      _: {
-        type: 'feeds.activity.removed_from_feed';
-      } & ActivityRemovedFromFeedEvent & {
-          type: 'feeds.activity.removed_from_feed';
-        },
-    ) => {},
-    'feeds.activity.updated': (
-      event: { type: 'feeds.activity.updated' } & ActivityUpdatedEvent,
-    ) => {
+    'feeds.activity.removed_from_feed': Feed.noop,
+    'feeds.activity.updated': (event) => {
       const currentActivities = this.state.getLatestValue().activities;
       if (currentActivities) {
         const result = updateActivityInState(event.activity, currentActivities);
@@ -161,149 +114,107 @@ export class Feed extends FeedApi {
         }
       }
     },
-    'feeds.bookmark.added': (
-      _: { type: 'feeds.bookmark.added' } & BookmarkAddedEvent,
-    ) => {},
-    'feeds.bookmark.deleted': (
-      _: { type: 'feeds.bookmark.deleted' } & BookmarkDeletedEvent,
-    ) => {},
-    'feeds.bookmark.updated': (
-      _: { type: 'feeds.bookmark.updated' } & BookmarkUpdatedEvent,
-    ) => {},
-    'feeds.comment.added': (
-      _: { type: 'feeds.comment.added' } & CommentAddedEvent,
-    ) => {},
-    'feeds.comment.deleted': (
-      _: { type: 'feeds.comment.deleted' } & CommentDeletedEvent,
-    ) => {},
-    'feeds.comment.updated': (
-      _: { type: 'feeds.comment.updated' } & CommentUpdatedEvent,
-    ) => {},
-    'feeds.feed.created': (
-      _: { type: 'feeds.feed.created' } & FeedCreatedEvent,
-    ) => {
-      // nothing to do
-    },
-    'feeds.feed.deleted': (
-      _: { type: 'feeds.feed.deleted' } & FeedDeletedEvent,
-    ) => {
-      // nothing to do
-    },
-    'feeds.feed.updated': (
-      event: { type: 'feeds.feed.updated' } & FeedUpdatedEvent,
-    ) => {
+    'feeds.bookmark.added': Feed.noop,
+    'feeds.bookmark.deleted': Feed.noop,
+    'feeds.bookmark.updated': Feed.noop,
+    'feeds.comment.added': Feed.noop,
+    'feeds.comment.deleted': Feed.noop,
+    'feeds.comment.updated': Feed.noop,
+    'feeds.feed.created': Feed.noop,
+    'feeds.feed.deleted': Feed.noop,
+    'feeds.feed.updated': (event) => {
       this.state.partialNext({ ...event.feed });
     },
-    'feeds.feed_group.changed': (
-      _: { type: 'feeds.feed_group.changed' } & FeedGroupChangedEvent,
-    ) => {},
-    'feeds.feed_group.deleted': (
-      _: { type: 'feeds.feed_group.deleted' } & FeedGroupDeletedEvent,
-    ) => {},
-    'feeds.follow.created': (
-      _: { type: 'feeds.follow.created' } & FollowCreatedEvent,
-    ) => {},
-    'feeds.follow.deleted': (
-      _: { type: 'feeds.follow.deleted' } & FollowDeletedEvent,
-    ) => {},
-    'feeds.follow.updated': (
-      _: { type: 'feeds.follow.updated' } & FollowUpdatedEvent,
-    ) => {},
-    'feeds.comment.reaction.added': (
-      _: {
-        type: 'feeds.comment.reaction.added';
-      } & CommentReactionAddedEvent & {
-          type: 'feeds.comment.reaction.added';
-        },
-    ) => {},
-    'feeds.comment.reaction.deleted': (
-      _: {
-        type: 'feeds.comment.reaction.deleted';
-      } & CommentReactionDeletedEvent & {
-          type: 'feeds.comment.reaction.deleted';
-        },
-    ) => {},
-    'feeds.feed_member.added': function (
-      _: { type: 'feeds.feed_member.added' } & FeedMemberAddedEvent & {
-          type: 'feeds.feed_member.added';
-        },
-    ): void {},
-    'feeds.feed_member.removed': function (
-      _: { type: 'feeds.feed_member.removed' } & FeedMemberRemovedEvent & {
-          type: 'feeds.feed_member.removed';
-        },
-    ): void {},
-    'feeds.feed_member.updated': function (
-      _: { type: 'feeds.feed_member.updated' } & FeedMemberUpdatedEvent & {
-          type: 'feeds.feed_member.updated';
-        },
-    ): void {},
-    'feeds.poll.closed': function (
-      _: { type: 'feeds.poll.closed' } & PollClosedFeedEvent & {
-          type: 'feeds.poll.closed';
-        },
-    ): void {},
-    'feeds.poll.deleted': function (
-      _: { type: 'feeds.poll.deleted' } & PollDeletedFeedEvent & {
-          type: 'feeds.poll.deleted';
-        },
-    ): void {},
-    'feeds.poll.updated': function (
-      _: { type: 'feeds.poll.updated' } & PollUpdatedFeedEvent & {
-          type: 'feeds.poll.updated';
-        },
-    ): void {},
-    'feeds.poll.vote_casted': function (
-      _: { type: 'feeds.poll.vote_casted' } & PollVoteCastedFeedEvent & {
-          type: 'feeds.poll.vote_casted';
-        },
-    ): void {},
-    'feeds.poll.vote_changed': function (
-      _: { type: 'feeds.poll.vote_changed' } & PollVoteChangedFeedEvent & {
-          type: 'feeds.poll.vote_changed';
-        },
-    ): void {},
-    'feeds.poll.vote_removed': function (
-      _: { type: 'feeds.poll.vote_removed' } & PollVoteRemovedFeedEvent & {
-          type: 'feeds.poll.vote_removed';
-        },
-    ): void {},
-    'feeds.activity.pinned': function (
-      _: { type: 'feeds.activity.pinned' } & ActivityPinnedEvent & {
-          type: 'feeds.activity.pinned';
-        },
-    ): void {},
-    'feeds.activity.unpinned': function (
-      _: { type: 'feeds.activity.unpinned' } & ActivityUnpinnedEvent & {
-          type: 'feeds.activity.unpinned';
-        },
-    ): void {},
-    'feeds.activity.marked': function (
-      _: { type: 'feeds.activity.marked' } & ActivityMarkEvent & {
-          type: 'feeds.activity.marked';
-        },
-    ): void {},
-    'moderation.custom_action': function (
-      _: { type: 'moderation.custom_action' } & ModerationCustomActionEvent & {
-          type: 'moderation.custom_action';
-        },
-    ): void {
-      throw new Error('Function not implemented.');
+    'feeds.feed_group.changed': Feed.noop,
+    'feeds.feed_group.deleted': Feed.noop,
+    'feeds.follow.created': (event) => {
+      if (event.follow.source_feed.fid === this.fid) {
+        // this feed followed someone
+        this.state.next((currentState) => ({
+          ...currentState,
+          following_count: (currentState.following_count ?? 0) + 1,
+          following: currentState.following
+            ? currentState.following.concat(event.follow)
+            : [event.follow],
+        }));
+
+        // add own follow to the target feed
+        const target = event.follow.target_feed;
+        const feed = this.client.feed(target.group_id, target.id);
+
+        feed.state.next((currentState) => ({
+          ...currentState,
+          own_follows: currentState.own_follows
+            ? currentState.own_follows.concat(event.follow)
+            : [event.follow],
+        }));
+      } else if (event.follow.target_feed.fid === this.fid) {
+        // someone followed this feed
+        this.state.next((currentState) => ({
+          ...currentState,
+          follower_count: (currentState.follower_count ?? 0) + 1,
+          followers: currentState.followers
+            ? currentState.followers.concat(event.follow)
+            : [event.follow],
+        }));
+      }
     },
-    'moderation.flagged': function (
-      _: { type: 'moderation.flagged' } & ModerationFlaggedEvent & {
-          type: 'moderation.flagged';
-        },
-    ): void {
-      throw new Error('Function not implemented.');
+    'feeds.follow.deleted': (event) => {
+      if (event.follow.source_feed.fid === this.fid) {
+        // this feed unfollowed someone
+        this.state.next((currentState) => {
+          return {
+            ...currentState,
+            following_count: currentState.following_count
+              ? currentState.following_count - 1
+              : 0,
+            following: currentState.following?.filter(
+              (follow) =>
+                follow.target_feed.fid !== event.follow.target_feed.fid,
+            ),
+          };
+        });
+
+        const target = event.follow.target_feed;
+        const feed = this.client.feed(target.group_id, target.id);
+
+        feed.state.next((currentState) => ({
+          ...currentState,
+          own_follows: currentState.own_follows?.filter(
+            (follow) => follow.target_feed.fid !== event.follow.target_feed.fid,
+          ),
+        }));
+      } else if (event.follow.target_feed.fid === this.fid) {
+        // someone unfollowed this feed
+        this.state.next((currentState) => ({
+          ...currentState,
+          follower_count: currentState.follower_count
+            ? currentState.follower_count - 1
+            : 0,
+          followers: currentState.followers?.filter(
+            (follow) => follow.source_feed.fid !== event.follow.source_feed.fid,
+          ),
+        }));
+      }
     },
-    'moderation.mark_reviewed': function (
-      _: { type: 'moderation.mark_reviewed' } & ModerationMarkReviewedEvent & {
-          type: 'moderation.mark_reviewed';
-        },
-    ): void {
-      throw new Error('Function not implemented.');
-    },
+    'feeds.follow.updated': Feed.noop,
+    'feeds.comment.reaction.added': Feed.noop,
+    'feeds.comment.reaction.deleted': Feed.noop,
+    'feeds.feed_member.added': Feed.noop,
+    'feeds.feed_member.removed': Feed.noop,
+    'feeds.feed_member.updated': Feed.noop,
+    'feeds.poll.closed': Feed.noop,
+    'feeds.poll.deleted': Feed.noop,
+    'feeds.poll.updated': Feed.noop,
+    'feeds.poll.vote_casted': Feed.noop,
+    'feeds.poll.vote_changed': Feed.noop,
+    'feeds.poll.vote_removed': Feed.noop,
+    'feeds.activity.pinned': Feed.noop,
+    'feeds.activity.unpinned': Feed.noop,
+    'feeds.activity.marked': Feed.noop,
+    'moderation.custom_action': Feed.noop,
+    'moderation.flagged': Feed.noop,
+    'moderation.mark_reviewed': Feed.noop,
   };
 
   protected eventDispatcher: EventDispatcher<WSEvent['type'], WSEvent> =
@@ -311,12 +222,15 @@ export class Feed extends FeedApi {
 
   constructor(
     client: FeedsClient,
-    group: string,
+    groupId: 'user' | 'timeline' | (string & {}),
     id: string,
     data?: FeedResponse,
   ) {
-    super(client, group, id);
+    super(client, groupId, id);
     this.state = new StateStore<FeedState>({
+      fid: `${groupId}:${id}`,
+      group_id: groupId,
+      id,
       ...(data ?? {}),
       is_loading: false,
       is_loading_activities: false,
@@ -330,11 +244,15 @@ export class Feed extends FeedApi {
     return `${this.group}:${this.id}`;
   }
 
+  get currentState() {
+    return this.state.getLatestValue();
+  }
+
   async getOrCreate(request?: GetOrCreateFeedRequest) {
     if (this.state.getLatestValue().is_loading_activities) {
       throw new Error('Only one getOrCreate call is allowed at a time');
     }
-    
+
     this.state.partialNext({
       is_loading: !request?.next,
       is_loading_activities: true,
@@ -343,8 +261,9 @@ export class Feed extends FeedApi {
     try {
       const response = await super.getOrCreate(request);
       if (request?.next) {
-        const currentState = this.state.getLatestValue();
-        const currentActivities = currentState.activities || [];
+        const { activities: currentActivities = [] } =
+          this.state.getLatestValue();
+
         const result = addActivitiesToState(
           response.activities,
           currentActivities,
@@ -395,6 +314,60 @@ export class Feed extends FeedApi {
     }
   }
 
+  /**
+   * Method which queries followers of this feed (feeds which target this feed).
+   *
+   * _Note: Useful only for feeds with `groupId` of `user` value._
+   */
+  async queryFollowers(request: Omit<QueryFollowsRequest, 'filter'>) {
+    const response = await this.client.queryFollows({
+      filter: {
+        target_feed: this.fid,
+      },
+      ...request,
+    });
+
+    return response;
+  }
+
+  /**
+   * Method which queries followings of this feed (target feeds of this feed).
+   *
+   * _Note: Useful only for feeds with `groupId` of `timeline` value._
+   */
+  async queryFollowings(request: Omit<QueryFollowsRequest, 'filter'>) {
+    const response = await this.client.queryFollows({
+      filter: {
+        source_feed: this.fid,
+      },
+      ...request,
+    });
+
+    return response;
+  }
+
+  async follow(feedOrFid: Feed | string) {
+    const fid = typeof feedOrFid === 'string' ? feedOrFid : feedOrFid.fid;
+
+    const response = await this.client.follow({
+      source: this.fid,
+      target: fid,
+    });
+
+    return response;
+  }
+
+  async unfollow(feedOrFid: Feed | string) {
+    const fid = typeof feedOrFid === 'string' ? feedOrFid : feedOrFid.fid;
+
+    const response = await this.client.unfollow({
+      source: this.fid,
+      target: fid,
+    });
+
+    return response;
+  }
+
   async getNextPage() {
     const currentState = this.state.getLatestValue();
     const response = await this.getOrCreate({
@@ -424,12 +397,16 @@ export class Feed extends FeedApi {
   off = this.eventDispatcher.off;
 
   handleWSEvent(event: WSEvent) {
-    const eventHandler: Function = this.eventHandlers[event.type];
-    if (eventHandler) {
+    const eventHandler = this.eventHandlers[event.type];
+
+    // no need to run noop function
+    if (typeof eventHandler === 'function' && eventHandler !== Feed.noop) {
+      // @ts-expect-error eventHandler's event parameter is not "never", TS is having a seizure for some reason
       eventHandler(event);
-    } else {
+    } else if (typeof eventHandler !== 'function') {
       console.warn(`Received unknown event type: ${event.type}`, event);
     }
+
     this.eventDispatcher.dispatch(event);
   }
 }
