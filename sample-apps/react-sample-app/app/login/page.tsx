77e0104eb8f677d8ed626e642f835f6105528244
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-07-11 14:45:02 +0200

    fix: bring back login page

---
+++ sample-apps/react-sample-app/app/login/page.tsx
@@ -0,0 +1,39 @@
+'use client';
+import type { UserRequest } from '@stream-io/feeds-client';
+import { useRouter } from 'next/navigation';
+import { useUserContext } from '../user-context';
+import { useErrorContext } from '../error-context';
+
+export default function Login() {
+  const { users, logIn } = useUserContext();
+  const { throwUnrecoverableError } = useErrorContext();
+  const router = useRouter();
+
+  const login = (user: UserRequest) => {
+    logIn(user).catch((err) => {
+      throwUnrecoverableError(err);
+    });
+    router.push('/timeline');
+  };
+
+  return (
+    <div>
+      <h2 className="text-4xl font-extrabold text-center">
+        Choose a user to log in
+      </h2>
+      <ul className="divide-y divide-gray-200">
+        {users.map((user) => (
+          <li key={user.id}>
+            <button
+              className="w-full h-full flex items-center gap-1 py-4"
+              onClick={() => login(user)}
+            >
+              <img className="size-10 rounded-full" src={user.image} alt="" />
+              <p className="text-sm font-medium text-gray-900">{user.name}</p>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
