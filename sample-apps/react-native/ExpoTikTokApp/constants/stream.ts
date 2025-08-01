daff3ce39271f982fa97f999f0ebe7050168cc0a
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-08-01 18:01:55 +0200

    chore: add more docs to the readme

--- sample-apps/react-native/ExpoTikTokApp/constants/stream.ts
+++ sample-apps/react-native/ExpoTikTokApp/constants/stream.ts
@@ -1,4 +1,3 @@
-// export const apiKey = 'fa5xpkvxrdw4';
 export const apiKey = 'ccqysy8u2tm6';
 export const tokenCreationUrl = 'https://pronto.getstream.io/api/auth/create-token?environment=feeds-react-native-sample-app';
 export const placesApiKey = process.env.EXPO_PUBLIC_PLACES_API_KEY;
