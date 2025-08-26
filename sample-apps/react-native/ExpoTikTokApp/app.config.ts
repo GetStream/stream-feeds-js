052e17c5ddd6059479fc8f7516eef1d6e657f6d0
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-08-26 15:31:47 +0200

    fix: map key resolution issue

---
+++ sample-apps/react-native/ExpoTikTokApp/app.config.ts
@@ -0,0 +1,68 @@
+// app.config.ts
+import type { ExpoConfig } from 'expo/config';
+
+const MAPS_KEY = process.env.EXPO_PUBLIC_MAPS_API_KEY ?? '';
+
+const config: ExpoConfig = {
+  name: 'ExpoTikTokApp',
+  slug: 'ExpoTikTokApp',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'expotiktokapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  splash: {
+    image: './assets/images/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    entitlements: { 'aps-environment': 'development' },
+    infoPlist: { UIBackgroundModes: ['remote-notification'] },
+    googleServicesFile: './firebase/GoogleService-Info.plist',
+    supportsTablet: true,
+    bundleIdentifier: 'io.getstream.expotiktokapp',
+    appleTeamId: 'EHV7XZLAHA',
+    config: {
+      googleMapsApiKey: MAPS_KEY,
+    },
+  },
+  android: {
+    googleServicesFile: './firebase/google-services.json',
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    package: 'io.getstream.expotiktokapp',
+    config: {
+      googleMaps: {
+        apiKey: MAPS_KEY,
+      },
+    },
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: [
+    'expo-router',
+    '@react-native-firebase/app',
+    '@react-native-firebase/messaging',
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          useFrameworks: 'static',
+        },
+      },
+    ],
+    './config-plugins/with-notifee-maven',
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+};
+
+export default config;
