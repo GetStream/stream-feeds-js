e3b6a4651a889e90aa23d3a6f0c887762589a0d5
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-08-28 13:23:39 +0200

    feat: add setup script to the rn app too

---
+++ sample-apps/react-native/ExpoTikTokApp/setup-env.js
@@ -0,0 +1,48 @@
+const { StreamClient } = require('@stream-io/node-sdk');
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+require('dotenv').config();
+
+(async () => {
+  const key = 'ccqysy8u2tm6';
+  const secret = process.env.STREAM_API_SECRET;
+
+  const users = JSON.parse(
+    await fs.readFile(path.resolve('users.json'), 'utf-8'),
+  );
+  const client = new StreamClient(key, secret);
+
+  console.log('Creating users...');
+  await client.upsertUsers(users);
+
+  console.log('Creating feeds for users...');
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+
+    const userFeed = client.feeds.feed('user', user.id);
+    await userFeed.getOrCreate({
+      visibility_level: user.visibility_level,
+      user: { id: user.id },
+    });
+
+    const timeline = client.feeds.feed('timeline', user.id);
+    await timeline.getOrCreate({
+      visibility_level: 'private',
+      user: { id: user.id },
+    });
+
+    // user's timeline follows user's post feed
+    await client.feeds.follow({
+      target: userFeed.feed,
+      source: timeline.feed,
+    });
+
+    await client.feeds.feed('notification', user.id).getOrCreate({
+      visibility_level: 'private',
+      user_id: user.id,
+    });
+  }
+
+  console.log('Finished initialization');
+})();
