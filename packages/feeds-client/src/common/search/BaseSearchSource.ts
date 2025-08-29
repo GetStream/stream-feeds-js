1b116213d773bc9baaed8b58168223522c43234d
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-08-29 15:42:16 +0200

    fix: switch to hasOwnProperty for older version support

--- packages/feeds-client/src/common/search/BaseSearchSource.ts
+++ packages/feeds-client/src/common/search/BaseSearchSource.ts
@@ -188,7 +188,7 @@ abstract class BaseSearchSourceBase<T> implements ISearchSource<T> {
     const { items, next } = result;
 
     const stateUpdate: Partial<SearchSourceState<T>> = {};
-    if (Object.hasOwn(result, 'next')) {
+    if (Object.prototype.hasOwnProperty.call(result, 'next')) {
       stateUpdate.next = next;
       stateUpdate.hasNext = !!next;
     } else {
