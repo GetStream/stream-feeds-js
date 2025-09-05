35a4619984b557505d8b4f13d83a18c4433fa81f
Zita Szupera <szuperaz@gmail.com>
2025-09-05 15:52:28 +0200

    Fix lint issue

--- packages/feeds-client/__integration-tests__/docs-snippets/push-preferences.test.ts
+++ packages/feeds-client/__integration-tests__/docs-snippets/push-preferences.test.ts
@@ -6,25 +6,18 @@ import {
 } from '../utils';
 import { FeedsClient } from '../../src/feeds-client';
 import { Feed } from '../../src/feed';
-import { ActivityResponse, UserRequest } from '../../src/gen/models';
+import { UserRequest } from '../../src/gen/models';
 
 describe('Push preferences page', () => {
   let client: FeedsClient;
   const user: UserRequest = getTestUser();
   let feed: Feed;
-  let activity: ActivityResponse;
 
   beforeAll(async () => {
     client = createTestClient();
     await client.connectUser(user, createTestTokenGenerator(user));
     feed = client.feed('user', crypto.randomUUID());
     await feed.getOrCreate({ watch: true });
-    activity = (
-      await feed.addActivity({
-        type: 'post',
-        text: 'Hello, world!',
-      })
-    ).activity;
   });
 
   it(`User-Level Feeds Preferences`, async () => {
