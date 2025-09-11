fa8c5c309c649ad618677eb45d1dfefb9068ccf7
Zita Szupera <szuperaz@gmail.com>
2025-09-11 17:09:16 +0200

    Remove unnecessary change

--- packages/feeds-client/__integration-tests__/utils.ts
+++ packages/feeds-client/__integration-tests__/utils.ts
@@ -73,7 +73,7 @@ export const waitForEvent = (
 ) => {
   return new Promise((resolve, reject) => {
     // @ts-expect-error client expects WSEvents
-    client.on(type, (e) => {
+    client.on(type, () => {
       resolve(undefined);
       clearTimeout(timeout);
     });
