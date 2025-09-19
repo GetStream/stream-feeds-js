a36cad9fee3c2de9bb2cfefe2c565a2defc3010f
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-09-19 14:59:52 +0200

    chore: update hook

--- packages/feeds-client/src/bindings/react/hooks/feed-state-hooks/useOwnCapabilities.ts
+++ packages/feeds-client/src/bindings/react/hooks/feed-state-hooks/useOwnCapabilities.ts
@@ -20,51 +20,65 @@ export const useOwnCapabilities = (feedFromProps?: Feed) => {
   const { oc = stableEmptyArray } = useStateStore(feed?.state, selector) ?? {};
 
   return useMemo(
-    () =>
-      ({
-        can_add_activity: oc.indexOf(FeedOwnCapability.ADD_ACTIVITY) > -1,
+    () => {
+      const capabilitiesSet = new Set(oc);
+      return ({
+        can_add_activity: capabilitiesSet.has(FeedOwnCapability.ADD_ACTIVITY),
+        can_add_activity_bookmark:
+          capabilitiesSet.has(FeedOwnCapability.ADD_ACTIVITY_BOOKMARK),
         can_add_activity_reaction:
-          oc.indexOf(FeedOwnCapability.ADD_ACTIVITY_REACTION) > -1,
-        can_add_comment: oc.indexOf(FeedOwnCapability.ADD_COMMENT) > -1,
+          capabilitiesSet.has(FeedOwnCapability.ADD_ACTIVITY_REACTION),
+        can_add_comment: capabilitiesSet.has(FeedOwnCapability.ADD_COMMENT),
         can_add_comment_reaction:
-          oc.indexOf(FeedOwnCapability.ADD_COMMENT_REACTION) > -1,
-        can_bookmark_activity:
-          oc.indexOf(FeedOwnCapability.BOOKMARK_ACTIVITY) > -1,
-        can_create_feed: oc.indexOf(FeedOwnCapability.CREATE_FEED) > -1,
-        can_delete_bookmark: oc.indexOf(FeedOwnCapability.DELETE_BOOKMARK) > -1,
-        can_delete_comment: oc.indexOf(FeedOwnCapability.DELETE_COMMENT) > -1,
-        can_delete_feed: oc.indexOf(FeedOwnCapability.DELETE_FEED) > -1,
-        can_edit_bookmark: oc.indexOf(FeedOwnCapability.EDIT_BOOKMARK) > -1,
-        can_follow: oc.indexOf(FeedOwnCapability.FOLLOW) > -1,
-        can_remove_activity: oc.indexOf(FeedOwnCapability.REMOVE_ACTIVITY) > -1,
-        can_remove_activity_reaction:
-          oc.indexOf(FeedOwnCapability.REMOVE_ACTIVITY_REACTION) > -1,
-        can_remove_comment_reaction:
-          oc.indexOf(FeedOwnCapability.REMOVE_COMMENT_REACTION) > -1,
-        can_unfollow: oc.indexOf(FeedOwnCapability.UNFOLLOW) > -1,
-        can_update_feed: oc.indexOf(FeedOwnCapability.UPDATE_FEED) > -1,
-        can_invite_feed: oc.indexOf(FeedOwnCapability.INVITE_FEED) > -1,
-        can_join_feed: oc.indexOf(FeedOwnCapability.JOIN_FEED) > -1,
-        can_leave_feed: oc.indexOf(FeedOwnCapability.LEAVE_FEED) > -1,
-        can_manage_feed_group:
-          oc.indexOf(FeedOwnCapability.MANAGE_FEED_GROUP) > -1,
-        can_mark_activity: oc.indexOf(FeedOwnCapability.MARK_ACTIVITY) > -1,
-        can_pin_activity: oc.indexOf(FeedOwnCapability.PIN_ACTIVITY) > -1,
+          capabilitiesSet.has(FeedOwnCapability.ADD_COMMENT_REACTION),
+        can_create_feed: capabilitiesSet.has(FeedOwnCapability.CREATE_FEED),
+        can_delete_any_activity:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_ANY_ACTIVITY),
+        can_delete_any_comment:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_ANY_COMMENT),
+        can_delete_feed: capabilitiesSet.has(FeedOwnCapability.DELETE_FEED),
+        can_delete_own_activity:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_OWN_ACTIVITY),
+        can_delete_own_activity_bookmark:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_OWN_ACTIVITY_BOOKMARK),
+        can_delete_own_activity_reaction:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_OWN_ACTIVITY_REACTION),
+        can_delete_own_comment:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_OWN_COMMENT),
+        can_delete_own_comment_reaction:
+          capabilitiesSet.has(FeedOwnCapability.DELETE_OWN_COMMENT_REACTION),
+        can_follow: capabilitiesSet.has(FeedOwnCapability.FOLLOW),
+        can_pin_activity: capabilitiesSet.has(FeedOwnCapability.PIN_ACTIVITY),
         can_query_feed_members:
-          oc.indexOf(FeedOwnCapability.QUERY_FEED_MEMBERS) > -1,
-        can_query_follows: oc.indexOf(FeedOwnCapability.QUERY_FOLLOWS) > -1,
-        can_read_activities: oc.indexOf(FeedOwnCapability.READ_ACTIVITIES) > -1,
-        can_read_feed: oc.indexOf(FeedOwnCapability.READ_FEED) > -1,
-        can_update_activity: oc.indexOf(FeedOwnCapability.UPDATE_ACTIVITY) > -1,
-        can_update_comment: oc.indexOf(FeedOwnCapability.UPDATE_COMMENT) > -1,
+          capabilitiesSet.has(FeedOwnCapability.QUERY_FEED_MEMBERS),
+        can_query_follows:
+          capabilitiesSet.has(FeedOwnCapability.QUERY_FOLLOWS),
+        can_read_activities:
+          capabilitiesSet.has(FeedOwnCapability.READ_ACTIVITIES),
+        can_read_feed: capabilitiesSet.has(FeedOwnCapability.READ_FEED),
+        can_unfollow: capabilitiesSet.has(FeedOwnCapability.UNFOLLOW),
+        can_update_any_activity:
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_ANY_ACTIVITY),
+        can_update_any_comment:
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_ANY_COMMENT),
+        can_update_feed: capabilitiesSet.has(FeedOwnCapability.UPDATE_FEED),
         can_update_feed_followers:
-          oc.indexOf(FeedOwnCapability.UPDATE_FEED_FOLLOWERS) > -1,
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_FEED_FOLLOWERS),
         can_update_feed_members:
-          oc.indexOf(FeedOwnCapability.UPDATE_FEED_MEMBERS) > -1,
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_FEED_MEMBERS),
+        can_update_own_activity:
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_OWN_ACTIVITY),
+        can_update_own_activity_bookmark:
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_OWN_ACTIVITY_BOOKMARK),
+        can_update_own_comment:
+          capabilitiesSet.has(FeedOwnCapability.UPDATE_OWN_COMMENT),
       }) satisfies Record<
-        `can_${KebabToSnakeCase<(typeof FeedOwnCapability)[keyof typeof FeedOwnCapability]>}`,
+        `can_${KebabToSnakeCase<
+          (typeof FeedOwnCapability)[keyof typeof FeedOwnCapability]
+        >}`,
         boolean
-      >,
+      >;
+    },
     [oc],
   );
 };
