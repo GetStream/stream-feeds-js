c591a266bb0e6d93ee4c64b41d1df092ab4c788d
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-09-30 18:35:01 +0200

    feat: add support for unique comment reactions

---
+++ packages/feeds-client/src/feed/event-handlers/comment/handle-comment-reaction-updated.ts
@@ -0,0 +1,76 @@
+import type { Feed } from '../../feed';
+import type { EventPayload } from '../../../types-internal';
+import { type PartializeAllBut } from '../../../types-internal';
+import { getStateUpdateQueueId, shouldUpdateState } from '../../../utils';
+
+export type CommentReactionUpdatedPayload = PartializeAllBut<
+  EventPayload<'feeds.comment.reaction.updated'>,
+  'comment' | 'reaction'
+>;
+
+export function handleCommentReactionUpdated(
+  this: Feed,
+  payload: CommentReactionUpdatedPayload,
+  fromWs?: boolean,
+) {
+  const { comment, reaction } = payload;
+  const connectedUser = this.client.state.getLatestValue().connected_user;
+
+  const isOwnReaction = reaction.user.id === connectedUser?.id;
+
+  if (
+    !shouldUpdateState({
+      stateUpdateQueueId: getStateUpdateQueueId(
+        payload,
+        'comment-reaction-updated',
+      ),
+      stateUpdateQueue: this.stateUpdateQueue,
+      watch: this.currentState.watch,
+      fromWs,
+      isTriggeredByConnectedUser: isOwnReaction,
+    })
+  ) {
+    return;
+  }
+
+  this.state.next((currentState) => {
+    const commentIndex = this.getCommentIndex(comment, currentState);
+
+    if (commentIndex === -1) return currentState;
+
+    const forId = comment.parent_id ?? comment.object_id;
+
+    const entityState = currentState.comments_by_entity_id[forId];
+
+    const newComments = entityState?.comments?.concat([]) ?? [];
+
+    let ownReactions = newComments[commentIndex].own_reactions;
+
+    ownReactions = [
+      ...ownReactions.filter(
+        (ownReaction) => ownReaction.type === reaction.type,
+      ),
+      reaction,
+    ];
+
+    newComments[commentIndex] = {
+      ...newComments[commentIndex],
+      reaction_count: comment.reaction_count ?? 0,
+      // TODO: FIXME this should be handled by the backend
+      latest_reactions: comment.latest_reactions ?? [],
+      reaction_groups: comment.reaction_groups ?? {},
+      own_reactions: ownReactions,
+    };
+
+    return {
+      ...currentState,
+      comments_by_entity_id: {
+        ...currentState.comments_by_entity_id,
+        [forId]: {
+          ...entityState,
+          comments: newComments,
+        },
+      },
+    };
+  });
+}
