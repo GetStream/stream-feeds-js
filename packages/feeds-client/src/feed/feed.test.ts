200ade940691c64101d41375ce675139858e8409
Zita Szupera <szuperaz@gmail.com>
2025-11-05 17:14:22 +0100

    fix: don't throw error if getOrCreate is called twice with same config (#160)
    🎫 Ticket:
    https://linear.app/stream/issue/REACT-634/dont-throw-error-if-getorcreate-is-called-twice-with-same-filter
    
    ### 💡 Overview
    
    Don't throw an error if `getOrCreate` is called, and a request is
    already in progress (with same config params).
    
    Calling `getOrCreate` can be an integration issue, or React's
    development mode.
    
    Follow up issue:
    https://linear.app/stream/issue/REACT-635/cancel-in-flight-getorcreate-requests-if
    ### 📝 Implementation notes

--- packages/feeds-client/src/feed/feed.test.ts
+++ packages/feeds-client/src/feed/feed.test.ts
@@ -1,4 +1,5 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { type Mock } from 'vitest';
 import { FeedsClient } from '../feeds-client';
 import { Feed } from './feed';
 import type { ActivityResponse } from '../gen/models';
@@ -87,6 +88,34 @@ describe('Feed derived state updates', () => {
     }
     expect(indexedActivityIdsBefore).toBe(indexedActivityIdsAfter);
   });
+});
+
+describe(`getOrCreate`, () => {
+  let feed: Feed;
+  let client: { [key in keyof FeedsClient]: Mock };
+
+  beforeEach(() => {
+    client = {
+      getOrCreateFeed: vi.fn().mockResolvedValue({
+        activities: [],
+        aggregated_activities: [],
+        members: [],
+        next: 'next',
+        prev: undefined,
+        limit: 10,
+        feed: generateFeedResponse({}),
+      }),
+      hydrateCapabilitiesCache: vi.fn(),
+      hydratePollCache: vi.fn(),
+    } as unknown as { [key in keyof FeedsClient]: Mock };
+    const feedResponse = generateFeedResponse({ id: 'main', group_id: 'user' });
+    feed = new Feed(
+      client as unknown as FeedsClient,
+      feedResponse.group_id,
+      feedResponse.id,
+      feedResponse,
+    );
+  });
 
   it(`should send all params from getOrCreate request that affect activity list when loading next page`, async () => {
     feed.state.partialNext({
@@ -124,17 +153,6 @@ describe('Feed derived state updates', () => {
       next: 'next',
     });
 
-    const spy = vi.spyOn(feed, 'getOrCreate').mockImplementation(() =>
-      // @ts-expect-error - mock implementation
-      Promise.resolve({
-        activities: [],
-        next: 'next',
-        prev: undefined,
-        limit: 10,
-        feed: generateFeedResponse({}),
-      }),
-    );
-
     await feed.getNextPage();
 
     const config = {
@@ -150,10 +168,54 @@ describe('Feed derived state updates', () => {
       following_pagination: {
         limit: 0,
       },
+      feed_group_id: feed.group,
+      feed_id: feed.id,
     };
-    const params = spy.mock.calls[0][0];
+    const params = client.getOrCreateFeed.mock.calls[0][0];
     expect(params).toStrictEqual(config);
+  });
 
-    spy.mockRestore();
+  it(`should prevent duplicate getOrCreate calls with same config`, () => {
+    const promise1 = feed.getOrCreate();
+    const promise2 = feed.getOrCreate();
+
+    expect(client.getOrCreateFeed).toHaveBeenCalledTimes(1);
+    expect(promise1).toEqual(promise2);
+  });
+
+  it(`should prevent duplicate getOrCreate calls with same config - non-empty filter`, () => {
+    const promise1 = feed.getOrCreate({
+      filter: { filter_tags: ['green'] },
+      watch: true,
+    });
+    const promise2 = feed.getOrCreate({
+      watch: true,
+      filter: { filter_tags: ['green'] },
+    });
+
+    expect(client.getOrCreateFeed).toHaveBeenCalledTimes(1);
+    expect(promise1).toEqual(promise2);
+  });
+
+  it(`should send request if getOrCreate is called serially`, async () => {
+    await feed.getOrCreate();
+    await feed.getOrCreate();
+
+    expect(client.getOrCreateFeed).toHaveBeenCalledTimes(2);
+  });
+
+  it(`should still throw error if we send two requests at the same time with different config (we will fix this later)`, () => {
+    feed.getOrCreate({ filter: { filter_tags: ['green'] } });
+    expect(
+      feed.getOrCreate({ filter: { filter_tags: ['blue'] } }),
+    ).rejects.toThrow('Only one getOrCreate call is allowed at a time');
+  });
+
+  it(`should still throw error if we send getOrCreate and getNextPage at the same time`, () => {
+    feed.state.partialNext({ next: 'next' });
+    feed.getNextPage();
+    expect(
+      feed.getOrCreate({ filter: { filter_tags: ['green'] } }),
+    ).rejects.toThrow('Only one getOrCreate call is allowed at a time');
   });
 });
